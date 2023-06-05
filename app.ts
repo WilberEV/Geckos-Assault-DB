781b154203e21fb58f9b39cdd4618f1b9c0be16d
WilberEV <wilberesparragoza@gmail.com>
2023-06-05 23:44:19 +0200

    app.ts initial layout

---
+++ app.ts
@@ -0,0 +1,33 @@
+//Imported values
+import express from "express";
+import mongoose from "mongoose";
+import config from './core/config.js';
+import cors from 'cors'
+
+
+
+const app = express();
+
+mongoose.connect(config.DB_URL)
+  .then(() => {
+    console.log("Conectado a la base de datos");
+  })
+  .catch((err) => {
+    console.log(err, "Problemas para conectar a la base de datos");
+});
+
+let corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  preflightContinue: false,
+  // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+  optionsSuccessStatus: 204
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.get('/',(req, res)=>(res.send('/////OK/////')))
+
+
+app.listen(config.PORT, () => console.log(`Servidor levantado en ${config.PORT}`));
