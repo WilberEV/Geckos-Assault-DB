0ab63076d8061d23b7eed7dde4e3f3b1a560f815
WilberEV <wilberesparragoza@gmail.com>
2023-06-06 12:27:34 +0200

    User Router initial layout

---
+++ entities/users/router.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import {Request, Response, NextFunction} from 'express';
+import {createUser, login, findUser, updateUser} from "./controler.js"
+import { auth } from "../../core/middlewares.js";
+
+const userRouter = express.Router()
+
+userRouter.post('/', async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.json(await createUser(req.body))
+    } catch(e){
+        next(e)
+    }
+});
+
+
+userRouter.post('/login', async (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        res.json(await login(req, res))
+    } catch(e){
+        next(e)
+    }
+}) 
+
+userRouter.get('/:id', auth, async (req: Request, res: Response, next: NextFunction) =>{
+    try{
+        res.json(await findUser(req.params.id, req.payload))
+    } catch(e){
+        next(e)
+    }
+})
+
+
+userRouter.put('/:id', auth, async (req: Request, res: Response, next: NextFunction) =>{
+    try{
+        res.json(await updateUser(req.params.id, req.body, req.payload))
+    } catch(e){
+        next(e)
+    }
+})
+
+export default userRouter
