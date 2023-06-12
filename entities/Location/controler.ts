672de4ac9365440e33cee51ed3002d4b87a2b849
WilberEV <wilberesparragoza@gmail.com>
2023-06-12 17:34:36 +0200

    Location: Create + Find location

---
+++ entities/Location/controler.ts
@@ -0,0 +1,22 @@
+import Location from "./model.js";
+
+
+/////Create Location/////
+export const createChara = async (data) => {
+    try {
+      const location = await Location.create(data);
+      return location;
+    } catch (err) {
+      throw new Error("INVALID_DATA");
+    }
+  };
+
+/////Find Location/////
+export const findLocation = async (X, Y) => {
+    try {
+        return await Location.find( { $and: [{ xCoordinate: X }, { yCoordinate: Y }]});
+    } catch (err) {
+      throw new Error("NOT_FOUND");
+    }
+  };
+  
