aa3ecff8baaca00d0e8b799fc97af709344318ca
WilberEV <wilberesparragoza@gmail.com>
2023-06-18 03:00:45 +0200

    Middleware: Error Handler

--- core/middlewares.ts
+++ core/middlewares.ts
@@ -17,3 +17,17 @@ export const auth = (req: Request, res: Response, next) =>{
         return next(new Error('INVALID_CREDENTIALS'))
     }
 }
+
+export const errorHandler = (err: Error | MongoServerError, req: Request, res: Response, next: NextFunction) =>{
+    if(err.message === 'NO_TOKEN') return res.status(401).json({error: 'NO_TOKEN'})
+    if(err.message === 'INVALID_CREDENTIALS') return res.status(403).json({error: 'INVALID_CREDENTIALS'})
+    if(err.message === 'NOT_FOUND') return res.status(404).json({error: 'NOT_FOUND'})
+    if(err.message === 'INVALID_PASSWORD') return res.status(422).json({error: 'INVALID_PASSWORD'})
+    if(err.message === 'NOT_AUTHORIZED') return res.status(401).json({error: 'NOT_AUTHORIZED'})
+    if(err.message === 'MISSING_DATA') return res.status(400).json({error: 'MISSING_DATA'})
+    
+    if(err instanceof MongoServerError && err.code === 11000) return res.status(422).json({error: 'DUPLICATE_ENTITY', entities: Object.keys(err.keyPatter)})
+    //Unknown Error
+    console.error(err);
+    return res.status(500).json({error: 'SERVER_ERROR', err})
+ }
